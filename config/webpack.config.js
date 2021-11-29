0a1853aa8b9668cf6c93f38658105ee7254cce1d
weizhibin <weizhibin.66@bytedance.com>
2021-11-29 17:30:36 +0800

    feat: 解析css和less
    Change-Id: I94608f96f12b804efa9933ac85fce5035e90bc20

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname,'../src/demo2/index.js'),
+  entry: path.resolve(__dirname, '../src/demo2/index.js'),
   output: {
     path: path.resolve(__dirname, '../src/demo2/dist'),
     filename: 'bundle.js',
@@ -14,7 +14,22 @@ module.exports = {
       use: {
         loader: "babel-loader",
       }
-    }]
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: "style-loader",
+        options: { injectType: "styleTag" } 
+      }, "css-loader"]
+    },
+    {
+      test: /\.less$/,
+      use: [{
+        loader: "style-loader",
+        options: { injectType: "styleTag" } 
+      }, "css-loader","less-loader"]
+    }
+    ]
   },
   cache: false,
 };
--- src/demo2/LoginPage.js
+++ src/demo2/LoginPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './index.less';
 
 const LoginPage = () => {
   return (
-    <h1>Hello Please Login</h1>
+    <div className="content">
+      <h1 className="title">Hello Please Login</h1>
+    </div>
   )
 }
 
