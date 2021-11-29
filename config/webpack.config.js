b9e5d97e71a3b5691db335927d94c646b907502a
weizhibin <weizhibin.66@bytedance.com>
2021-11-29 19:03:10 +0800

    feat: 文件监听
    Change-Id: Ia7cb0b8a59f86dfbb4991e636e85b5bb4775f253

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -19,15 +19,15 @@ module.exports = {
       test: /\.css$/,
       use: [{
         loader: "style-loader",
-        options: { injectType: "styleTag" } 
+        options: { injectType: "styleTag" }
       }, "css-loader"]
     },
     {
       test: /\.less$/,
       use: [{
         loader: "style-loader",
-        options: { injectType: "styleTag" } 
-      }, "css-loader","less-loader"]
+        options: { injectType: "styleTag" }
+      }, "css-loader", "less-loader"]
     },
     {
       test: /\.(png|svg|jpg|gif)$/,
@@ -36,4 +36,12 @@ module.exports = {
     ]
   },
   cache: false,
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/, // 不监听的文件
+    aggregateTimeout: 300, // 监听到变化后，延迟 300ms 后执行，默认 300ms
+    poll: 1000, // 每秒检查一次变动
+  }
 };
+
+  
--- src/demo2/LoginPage.js
+++ src/demo2/LoginPage.js
@@ -5,7 +5,7 @@ import ReactIcon from './react.png';
 const LoginPage = () => {
   return (
     <div className="content">
-      <h1 className="title">Hello Please Login</h1>
+      <h1 className="title">Hello Please Login new????</h1>
       <img src={ReactIcon} />
     </div>
   )
