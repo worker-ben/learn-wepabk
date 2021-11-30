964ba446034475ab28c005fa7d5a52b9a78218b9
weizhibin <weizhibin.66@bytedance.com>
2021-11-30 16:20:33 +0800

    feat: 文件指纹
    Change-Id: Iddc496a8793b0ba0992d40f651637b1b8c01ba54

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/demo2/index.js'),
   output: {
     path: path.resolve(__dirname, '../src/demo2/dist'),
-    filename: 'bundle.js',
+    filename: 'bundle_[chunkhash:8].js',
   },
   mode: 'development',
   module: {
@@ -17,31 +18,35 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: [{
-        loader: "style-loader",
-        options: { injectType: "styleTag" }
-      }, "css-loader"]
+      use: [MiniCssExtractPlugin.loader, "css-loader"]
     },
     {
       test: /\.less$/,
-      use: [{
-        loader: "style-loader",
-        options: { injectType: "styleTag" }
-      }, "css-loader", "less-loader"]
+      use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
     },
     {
       test: /\.(png|svg|jpg|gif)$/,
-      use: ['file-loader']
+      use: [{
+        loader: "file-loader",
+        options: {
+          name: 'img/[name]_[hash].[ext]'
+        }
+      }]
     }
     ]
   },
+  plugins:[
+    new MiniCssExtractPlugin({
+      filename: "[name]_[contenthash:8].css"
+    }),
+  ],
   cache: false,
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/, // 不监听的文件
-    aggregateTimeout: 300, // 监听到变化后，延迟 300ms 后执行，默认 300ms
-    poll: 1000, // 每秒检查一次变动
-  },
+  // watch: true,
+  // watchOptions: {
+  //   ignored: /node_modules/, // 不监听的文件
+  //   aggregateTimeout: 300, // 监听到变化后，延迟 300ms 后执行，默认 300ms
+  //   poll: 1000, // 每秒检查一次变动
+  // },
   devServer: {
     static: {
       directory: path.resolve(__dirname, '../src/demo2/dist'),
