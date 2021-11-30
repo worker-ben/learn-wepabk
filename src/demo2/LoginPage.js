9323eac63268f7148e695853eafcff430106a04c
weizhibin <weizhibin.66@bytedance.com>
2021-11-30 10:11:37 +0800

    feat: 热更新
    Change-Id: I26a0665f3f2f3e929d5b1958cbdee840b433946e

--- src/demo2/LoginPage.js
+++ src/demo2/LoginPage.js
@@ -5,7 +5,7 @@ import ReactIcon from './react.png';
 const LoginPage = () => {
   return (
     <div className="content">
-      <h1 className="title">Hello Please Login new????</h1>
+      <h1 className="title">Hello Please Login new你好吗778</h1>
       <img src={ReactIcon} />
     </div>
   )
